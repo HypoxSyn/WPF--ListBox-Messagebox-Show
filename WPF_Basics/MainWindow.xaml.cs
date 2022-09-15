478cc9fa800cdf273a053d7b077cafc1dfb93d26
Chaz Hall <chaz.h.hall@gmail.com>
2022-09-15 14:29:05 -0600

    Add project files.

---
+++ WPF_Basics/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_Basics
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public void ListBoxItem1_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+
+            MessageBox.Show("Here is a List pop up.");
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+    }
+}
